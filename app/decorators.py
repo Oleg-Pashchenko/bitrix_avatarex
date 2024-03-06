6bb80297fba313affedc0a6fff33964244608a08
olehandro <odpash.itmo@gmail.com>
2024-03-07 00:55:20 +0300

    added docker and requirements

--- app/decorators.py
+++ app/decorators.py
@@ -8,6 +8,7 @@ from aiohttp import web
 async def request_processing(func):
     @wraps(func)
     async def wrapper(request):
+        print('yes')
         start_time = time.time()
         try:
             data = dict(json.loads(await request.text()))
