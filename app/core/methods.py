c350af7d27472acfa09731bbb41f21b1a89b9b1f
olehandro <odpash.itmo@gmail.com>
2024-03-26 15:31:47 +0300

    added docker and requirements

--- app/core/methods.py
+++ app/core/methods.py
@@ -7,13 +7,10 @@ async def new_message(data: NewMessageData):
     btx_hook = database.get_btx_hook(data.application_token)
     print(btx_hook)
     btx = BitrixAvatarex(webhook=btx_hook)
-    if 'data[PARAMS][MESSAGE_TYPE]' == 'L':
-        print('tyt')
-        deal = await btx.get_lead(data.lead_id)
-    else:
-        print('tam')
-        deal = await btx.get_deal(data.deal_id)
-    print(data)
+
+    deal = await btx.get_lead(data.lead_id)
+
+    print(deal)
     pipeline, status = bitrix.get_pipeline_and_status(deal)
     fields_info = await btx.get_all_fields()
     database.add_bitrix_message(data.message, pipeline, status, data.application_token, btx_hook, data.dialog_id,
